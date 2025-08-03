e88d3557f75e7f0aaaf976c23d043e082df820fe
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-08-03 12:43:55 +0200

    Add hamburger menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import {NavLink} from "react-router";
 
 
@@ -12,9 +13,39 @@ const navigationLinks = [
 ];
 
 export const Navigation = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <>
             <nav className="sticky top-0 z-10 bg-primary text-white">
+                <div className="flex justify-between items-center p-4">
+                    <img src="" alt="Logo"
+                         // TODO: This logo behaviour is only for main page, on other pages logo should always be visible
+                         className={
+                             `transition-all duration-300 ease-out w-16
+                            ${menuOpen ? "translate-x-0 opacity-100" : "-translate-x-10 opacity-0"}`
+                         }
+                    />
+                    <div
+                        className="flex flex-col justify-center items-center w-10 h-10 cursor-pointer group"
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <span className={
+                            `block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out
+                            ${menuOpen ? "rotate-45 translate-y-3" : ""}`
+                        }></span>
+
+                        <span className={`
+                            block w-8 h-1 bg-white rounded my-2 transition-all duration-300 ease-in-out 
+                            ${menuOpen ? "opacity-0" : ""}`
+                        }></span>
+
+                        <span className={
+                            `block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out 
+                            ${menuOpen ? "-rotate-45 -translate-y-3" : ""}`
+                        }></span>
+                    </div>
+                </div>
                 <div className={
                     `text-center font-text text-3xl font-bold flex flex-col overflow-hidden
                     transition-all duration-300 ease-in-out
