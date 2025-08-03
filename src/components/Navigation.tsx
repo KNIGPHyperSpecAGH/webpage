a2702cdc86a423611f00502b2c694afee579aa20
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-08-03 14:07:31 +0200

    Update navigation animations

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -25,17 +25,16 @@ export const Navigation = ({showLogo = true}: NavigationProps) => {
                 <div className="flex justify-between items-center p-4">
                     <NavLink
                         to="/"
+                        className={
+                            `z-12 transition-all duration-300 ease-out w-16
+                            ${showLogo || menuOpen ? "opacity-100" : "opacity-0"}`
+                        }
                         onClick={() => setMenuOpen(false)}
                     >
-                        <img src="" alt="Logo"
-                             className={
-                                 `transition-all duration-100 ease-out w-16
-                                ${showLogo || menuOpen ? "opacity-100" : "opacity-0"}`
-                             }
-                        />
+                        <img src="" alt="Logo"/>
                     </NavLink>
                     <div
-                        className="flex flex-col justify-center items-center w-10 h-10 cursor-pointer group"
+                        className="z-12 flex flex-col justify-center items-center w-10 h-10 cursor-pointer group"
                         onClick={() => setMenuOpen(!menuOpen)}
                     >
                         <span className={
@@ -54,25 +53,30 @@ export const Navigation = ({showLogo = true}: NavigationProps) => {
                         }></span>
                     </div>
                 </div>
-                <div className={
-                    `text-center font-text text-3xl font-bold flex flex-col overflow-hidden
-                    transition-all duration-300 ease-in-out
-                    ${menuOpen ? "max-h-screen pb-4" : "max-h-0 p-0"}`
-                }>
+                <div
+                    className={
+                        `fixed top-0 left-0 z-11 w-full h-screen bg-primary flex flex-col pt-24
+                         text-center font-text text-3xl font-bold
+                         transition-all duration-300 ease-out origin-top
+                        ${menuOpen ? "opacity-100 scale-y-100" : "opacity-0 pointer-events-none scale-y-0"}`
+                    }
+                >
                     {navigationLinks.map((link, index) => (
                         <NavLink
                             key={link.path}
                             to={link.path}
                             className={
-                                `p-4 transition-all duration-300 ease-out hover:text-accent
-                                ${menuOpen ? "translate-x-0 opacity-100" : "-translate-x-10 opacity-0"}`
+                                `p-4 transition-all ease-out hover:text-accent
+                                ${menuOpen ? "translate-x-0 opacity-100 duration-300" : "-translate-x-10 opacity-0 duration-50"}`
                             }
-                            style={{transitionDelay: `${index * 50}ms`}}
+                            style={{transitionDelay: `${menuOpen ? (index * 50) + 100 : 0}ms`}}
                             onClick={() => setMenuOpen(false)}
                         >
                             {link.label}
                         </NavLink>
                     ))}
+
+                    <hr className="m-10 mt-5"/>
                 </div>
             </nav>
         </>
