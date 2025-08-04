d31e3a8f8fb03987f57751d57c97acc1f95efad8
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-08-04 12:32:06 +0200

    fix position of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer: React.FC = () => (
-    <footer className="bg-black text-gray-300 py-6 mt-8 w-full fixed bottom-0 left-0">
+    <footer className="bg-black text-white py-6 mt-8 w-full">
         <div className="container mx-auto px-4 flex flex-col items-center space-y-4">
             <span className="text-sm font-text text-white">&copy; {new Date().getFullYear()} HyperSpec AGH. All rights reserved.</span>
         </div>
