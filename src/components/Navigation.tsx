3054390c5a9220947fb73dcb7b44eedd2b1e38d4
unknown <kamilr2000@gmail.com>
2025-08-05 18:57:06 +0200

    Add Socials component and integrate it into navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,84 +1,94 @@
-import {useState} from "react";
-import {NavLink} from "react-router";
-
+import { useState } from "react";
+import { NavLink } from "react-router";
+import { Socials } from "./Socials.tsx";
 
 const navigationLinks = [
-    // {path: "/", label: "strona glowna"},
-    {path: "aktualnosci", label: "Aktualności"},
-    {path: "projekty", label: "Projekty"},
-    {path: "o-nas", label: "O nas"},
-    {path: "osiagniecia", label: "Osiągnięcia"},
-    {path: "kontakt", label: "Kontakt"},
-    {path: "sponsorzy", label: "Sponsorzy"},
+  // {path: "/", label: "strona glowna"},
+  { path: "aktualnosci", label: "Aktualności" },
+  { path: "projekty", label: "Projekty" },
+  { path: "o-nas", label: "O nas" },
+  { path: "osiagniecia", label: "Osiągnięcia" },
+  { path: "kontakt", label: "Kontakt" },
+  { path: "sponsorzy", label: "Sponsorzy" },
 ];
 
 interface NavigationProps {
-    showLogo?: boolean
+  showLogo?: boolean;
 }
 
-export const Navigation = ({showLogo = true}: NavigationProps) => {
-    const [menuOpen, setMenuOpen] = useState(false);
+export const Navigation = ({ showLogo = true }: NavigationProps) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    return (
-        <>
-            <nav className="sticky top-0 z-10 bg-primary text-white">
-                <div className="flex justify-between items-center p-4">
-                    <NavLink
-                        to="/"
-                        className={
-                            `z-12 transition-all duration-300 ease-out w-16
-                            ${showLogo || menuOpen ? "opacity-100" : "opacity-0"}`
-                        }
-                        onClick={() => setMenuOpen(false)}
-                    >
-                        <img src="" alt="Logo"/>
-                    </NavLink>
-                    <div
-                        className="z-12 flex flex-col justify-center items-center w-10 h-10 cursor-pointer group"
-                        onClick={() => setMenuOpen(!menuOpen)}
-                    >
-                        <span className={
-                            `block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out
-                            ${menuOpen ? "rotate-45 translate-y-3" : ""}`
-                        }></span>
+  return (
+    <>
+      <nav className="sticky top-0 z-10 bg-primary text-white">
+        <div className="flex justify-between items-center p-4">
+          <NavLink
+            to="/"
+            className={`z-12 transition-all duration-300 ease-out w-16
+                            ${
+                              showLogo || menuOpen ? "opacity-100" : "opacity-0"
+                            }`}
+            onClick={() => setMenuOpen(false)}
+          >
+            <img src="" alt="Logo" />
+          </NavLink>
+          <div
+            className="z-12 flex flex-col justify-center items-center w-10 h-10 cursor-pointer group"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span
+              className={`block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out
+                            ${menuOpen ? "rotate-45 translate-y-3" : ""}`}
+            ></span>
 
-                        <span className={`
+            <span
+              className={`
                             block w-8 h-1 bg-white rounded my-2 transition-all duration-300 ease-in-out 
-                            ${menuOpen ? "opacity-0" : ""}`
-                        }></span>
+                            ${menuOpen ? "opacity-0" : ""}`}
+            ></span>
 
-                        <span className={
-                            `block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out 
-                            ${menuOpen ? "-rotate-45 -translate-y-3" : ""}`
-                        }></span>
-                    </div>
-                </div>
-                <div
-                    className={
-                        `fixed top-0 left-0 z-11 w-full h-screen bg-primary flex flex-col pt-24
+            <span
+              className={`block w-8 h-1 bg-white rounded transition-all duration-300 ease-in-out 
+                            ${menuOpen ? "-rotate-45 -translate-y-3" : ""}`}
+            ></span>
+          </div>
+        </div>
+        <div
+          className={`fixed top-0 left-0 z-11 w-full h-screen bg-primary flex flex-col pt-24
                          text-center font-text text-3xl font-bold
                          transition-all duration-300 ease-out origin-top
-                        ${menuOpen ? "opacity-100 scale-y-100" : "opacity-0 pointer-events-none scale-y-0"}`
-                    }
-                >
-                    {navigationLinks.map((link, index) => (
-                        <NavLink
-                            key={link.path}
-                            to={link.path}
-                            className={
-                                `p-4 transition-all ease-out hover:text-accent
-                                ${menuOpen ? "translate-x-0 opacity-100 duration-300" : "-translate-x-10 opacity-0 duration-50"}`
-                            }
-                            style={{transitionDelay: `${menuOpen ? (index * 50) + 100 : 0}ms`}}
-                            onClick={() => setMenuOpen(false)}
-                        >
-                            {link.label}
-                        </NavLink>
-                    ))}
+                        ${
+                          menuOpen
+                            ? "opacity-100 scale-y-100"
+                            : "opacity-0 pointer-events-none scale-y-0"
+                        }`}
+        >
+          {navigationLinks.map((link, index) => (
+            <NavLink
+              key={link.path}
+              to={link.path}
+              className={`p-4 transition-all ease-out hover:text-accent
+                                ${
+                                  menuOpen
+                                    ? "translate-x-0 opacity-100 duration-300"
+                                    : "-translate-x-10 opacity-0 duration-50"
+                                }`}
+              style={{
+                transitionDelay: `${menuOpen ? index * 50 + 100 : 0}ms`,
+              }}
+              onClick={() => setMenuOpen(false)}
+            >
+              {link.label}
+            </NavLink>
+          ))}
 
-                    <hr className="m-10 mt-5"/>
-                </div>
-            </nav>
-        </>
-    );
+          <hr className="m-5 mt-5" />
+
+          {/* Socials component */}
+          <Socials />
+        </div>
+      </nav>
+    </>
+  );
 };
