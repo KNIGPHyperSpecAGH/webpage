a575e9d66e2eaf5f2aba46fbd89260d9a1313baf
fyrtax <33764198+fyrtax@users.noreply.github.com>
2025-08-06 19:54:59 +0200

    Fix nav button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,16 +5,16 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     active?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, active, ...props }) => (
+const Button: React.FC<ButtonProps> = ({children, active, ...props}) => (
     <button
         {...props}
         className={
-            `rounded-full text-lg px-6 py-2 border-white font-semibold text-white cursor-pointer text-base font-header transition-colors duration-200 hover:text-accent hover:border-accent
-            ${active ? "border-4 text-primary border-accent" : ""}`
+            `rounded-full text-lg px-6 py-2 font-semibold cursor-pointer transition-colors duration-200 hover:text-accent hover:border-accent border-4 whitespace-nowrap
+            ${active ? "border-accent" : "text-white border-transparent"}`
         }
     >
         {children}
     </button>
 );
 
-export { Button };
+export {Button};
