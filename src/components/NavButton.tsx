4afb756c0c32612bbb58dbfd3c54e9b911456932
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-08-17 12:22:18 +0200

    fix responsivness of NavButton

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -8,8 +8,10 @@ type ButtonProps = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement> & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 const NavButton: React.FC<ButtonProps> = ({ children, active, href, ...props }) => {
-    const baseClasses = `bg-accent text-white font-bold font-text rounded-full
-        px-8 py-4 text-xl shadow-lg hover:bg-accent/90 transition self-center hover:cursor-pointer
+    const baseClasses = `inline-flex items-center justify-center min-w-50
+        px-4 md:px-8 py-2 md:py-4 text-lg md:text-xl rounded-full
+        bg-accent text-white font-bold rounded-full shadow-lg transition
+        overflow-hidden whitespace-nowrap truncate
         ${active ? 'ring-2 ring-accent/60 scale-[0.99]' : ''}`;
     return (
         <NavLink to={href || '#'} className={baseClasses} {...(props as any)}>
