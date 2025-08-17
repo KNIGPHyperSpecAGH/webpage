8944f33cdcab9aea8c61420ed80a5f07692df157
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-08-17 19:19:49 +0200

    add whiteLogo.png, change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import {Socials} from "./Socials.tsx";
+import whiteLogo from "../assets/whiteLogo.png";
 
 const Footer: React.FC = () => (
     <footer className="bg-black text-white py-6  w-full">
-        <div className="container mx-auto px-4 flex flex-col items-center space-y-4">
-            <Socials/>
-            <span className="text-sm font-text text-white">&copy; {new Date().getFullYear()} HyperSpec AGH. All rights reserved.</span>
+        <div className="container mx-auto px-4 flex flex-row items-center space-y-4">
+            <span className="w-1/4">
+            <img src={whiteLogo} alt="Logo" />
+
+            </span>
+            <span className="flex flex-col w-1/4 ">
+                <h3 className="text-xl font-bold text-footer">Kontakt</h3>
+                <p className="mt-2"> e-mail: hyperspec@agh.edu.pl</p>
+            </span>
+            <span className="flex flex-col w-1/4 ">
+                <h3 className="text-xl font-bold text-footer">Social Media</h3>
+                <Socials />
+            </span>
+            <span className="w-1/4 text-sm font-text text-white">&copy; {new Date().getFullYear()} HyperSpec AGH. All rights reserved.</span>
             
         </div>
     </footer>
