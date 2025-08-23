4f2a579ab73c5d12a549b8b1993349b0cc219ed8
dawidwys31 <wysokinskidawid1@o2.pl>
2025-08-23 10:44:37 +0200

    Change Project Page styling to match site design

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { NavButton } from "./NavButton";
+import {NavButton} from "./NavButton";
 
 type ProjectCardProps = {
     title: string;
@@ -12,43 +12,45 @@ type ProjectCardProps = {
 };
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
-    title,
-    description,
-    image,
-    imageAlt = "",
-    variant = "left",
-    ctaLabel,
-    ctaHref,
-}) => {
+                                                     title,
+                                                     description,
+                                                     image,
+                                                     imageAlt = "",
+                                                     variant = "left",
+                                                     ctaLabel,
+                                                     ctaHref,
+                                                 }) => {
     const isRight = variant === "right";
 
     return (
         <article
             className={`w-full max-w-7xl mx-auto shadow-md overflow-hidden
-                        rounded-lg bg-element
-                        flex flex-col-reverse md:flex-row ${isRight ? "md:flex-row-reverse" : "md:flex-row"}
-                        `}
+                  rounded-[3vw] bg-element
+                  flex flex-col-reverse md:flex-row ${isRight ? "md:flex-row-reverse" : "md:flex-row"}`}
         >
-            {/* content */}
-            <div className="p-4 md:p-6 flex-1 flex flex-col justify-between">
+            <div
+                className="p-4 md:p-6 flex-1 flex flex-col justify-between items-center md:items-start text-center md:text-left">
                 <div>
-                    <h3 className="text-xl md:text-2xl font-semibold mb-3">
+                    <h3 className="text-xl md:text-2xl font-header mb-3 text-white">
                         {title}
                     </h3>
-                    <p className="text-lg md:text-xl leading-relaxed">
+                    <p className="text-lg md:text-xl font-text leading-relaxed">
                         {description}
                     </p>
                 </div>
 
-                {ctaLabel ? (
-                    <div className="mt-4">
-                        {ctaHref ? <NavButton href={ctaHref}>{ctaLabel}</NavButton>:<NavButton>{ctaLabel}</NavButton>}
+                {ctaLabel && (
+                    <div className="mt-4 flex w-full md:w-auto justify-center md:justify-start">
+                        {ctaHref ? (
+                            <NavButton href={ctaHref}>{ctaLabel}</NavButton>
+                        ) : (
+                            <NavButton>{ctaLabel}</NavButton>
+                        )}
                     </div>
-                ) : null}
+                )}
             </div>
 
-            {/* image */}
-            {image ? (
+            {image && (
                 <div className="w-full md:w-1/3 flex-shrink-0">
                     <img
                         src={image}
@@ -56,9 +58,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
                         className="w-full h-44 md:h-full object-cover"
                     />
                 </div>
-            ) : null}
+            )}
         </article>
     );
 };
 
-export { ProjectCard };
+export {ProjectCard};
