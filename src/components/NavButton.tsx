d6ea34a0b06402d75c74a55b3d980e666d6eea04
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-08-26 18:52:36 +0200

    change padding of navbutton

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -11,7 +11,7 @@ const NavButton: React.FC<ButtonProps> = ({ children, active, href, ...props }) 
     const baseClasses = `inline-flex items-center justify-center min-w-50
         px-4 md:px-8 py-2 md:py-4 text-lg md:text-xl rounded-full
         bg-accent text-white font-bold rounded-full shadow-lg transition
-        overflow-hidden whitespace-nowrap truncate
+        overflow-hidden whitespace-nowrap truncate mt-5
         ${active ? 'ring-2 ring-accent/60 scale-[0.99]' : ''}`;
     return (
         <NavLink to={href || '#'} className={baseClasses} {...(props as any)}>
