cf6641a7bfab3a70f511b844869268bf0c267360
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-09-19 18:47:22 +0200

    add github to links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,6 +1,7 @@
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const Socials = () => (
   <div className="flex justify-center space-x-3">
@@ -40,7 +41,7 @@ export const Socials = () => (
         className="transition-colors"
       />
     </a>
-    <a
+        <a
       href="https://www.linkedin.com/company/hyperspec"
       target="_blank"
       rel="noopener noreferrer"
@@ -49,6 +50,24 @@ export const Socials = () => (
       style={{ fontSize: "3rem" }}
     >
       <LinkedInIcon
+        sx={{
+          fontSize: "inherit",
+          color: "var(--color-white)",
+          transition: "color 0.2s",
+          ".group:hover &": { color: "var(--color-accent)" },
+        }}
+        className="transition-colors"
+      />
+    </a>
+    <a
+      href="https://github.com/KNIGPHyperSpecAGH/webpage"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="GitHub"
+      className="group flex items-center justify-center"
+      style={{ fontSize: "3rem" }}
+    >
+      <GitHubIcon
         sx={{
           fontSize: "inherit",
           color: "var(--color-white)",
